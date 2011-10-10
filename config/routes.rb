e5913902aa93bbd3a73ac544ef17fe0b9bf8dfc4
Neftali Alverio <nalverio@gmail.com>
2011-10-09 22:45:02 -0400

    modified routes to make main#index the first landing page

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ CliqSynq::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'main#index', :as => 'main'
 
   # See how all your routes lay out with "rake routes"
 
